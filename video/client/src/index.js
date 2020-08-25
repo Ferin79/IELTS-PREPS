d11a2e1eb9984d21f64c6bfe0e6e3727b4ef1f1c
Ferin <ferin@localhost.localdomain>
2020-08-25 17:12:29 +0530

    Changes to video/public

--- video/client/src/index.js
+++ video/client/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
