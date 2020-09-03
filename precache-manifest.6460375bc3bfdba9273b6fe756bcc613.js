a922b656cba85c07789e18c83889fe701a44d08a
Ferin <ferin@localhost.localdomain>
2020-09-03 12:42:22 +0530

    Updates

---
+++ precache-manifest.6460375bc3bfdba9273b6fe756bcc613.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d071d76d94b9fa0567cef63d4ba97dfe",
+    "url": "/IELTS-PREPS/index.html"
+  },
+  {
+    "revision": "db4320dbddb853bd18b7",
+    "url": "/IELTS-PREPS/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "59fdc5d87730f302f3a4",
+    "url": "/IELTS-PREPS/static/css/main.0bfc69e6.chunk.css"
+  },
+  {
+    "revision": "db4320dbddb853bd18b7",
+    "url": "/IELTS-PREPS/static/js/2.0a1e6d01.chunk.js"
+  },
+  {
+    "revision": "b5379487be8eaa54281f5714f7d8ca39",
+    "url": "/IELTS-PREPS/static/js/2.0a1e6d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59fdc5d87730f302f3a4",
+    "url": "/IELTS-PREPS/static/js/main.bb8c5758.chunk.js"
+  },
+  {
+    "revision": "9f1be9b5b44bab970305",
+    "url": "/IELTS-PREPS/static/js/runtime-main.6b101641.js"
+  },
+  {
+    "revision": "76e66448a0fc549a769afa5f19f97d90",
+    "url": "/IELTS-PREPS/static/media/world .76e66448.png"
+  }
+]);
