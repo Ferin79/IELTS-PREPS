ba144668e2eb7341277f77ac41c03137ec06c687
JASH-JARIWALA <jashjariwala7911@gmail.com>
2020-10-02 18:14:56 +0530

    heroku setup

--- web/src/pages/VideoCall/VideoCall-OldUI-OldLogic.jsx
+++ web/src/pages/VideoCall/VideoCall-OldUI-OldLogic.jsx
@@ -58,8 +58,8 @@ function VideoCall() {
 
   useEffect(() => {
     // 1. connect to server
-    socket.current = io.connect("http://localhost:8000/");
-    // socket.current = io.connect("");
+    // socket.current = io.connect("http://localhost:8000/");
+    socket.current = io.connect("");
     navigator.mediaDevices.getUserMedia({ video: { facingMode: cameraMode }, audio: true }).then((stream) => {
         setStream(stream);
         if (userVideo.current) {
