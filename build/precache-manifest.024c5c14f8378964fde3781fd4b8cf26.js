cb6472b91fad779189dca53022d7566e9b0284e2
JASH-JARIWALA <jashjariwala7911@gmail.com>
2020-10-14 15:50:17 +0530

    build files moved

---
+++ build/precache-manifest.024c5c14f8378964fde3781fd4b8cf26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594ba2bc7ff5062f9f0e40d238f14afd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "819cf5dd508de6e98ab7",
+    "url": "/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "18a73e08d2ccae1fda7c",
+    "url": "/static/css/main.3b952aef.chunk.css"
+  },
+  {
+    "revision": "819cf5dd508de6e98ab7",
+    "url": "/static/js/2.9928e8dc.chunk.js"
+  },
+  {
+    "revision": "6ce1330154a001ca508a3c163f47388a",
+    "url": "/static/js/2.9928e8dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a73e08d2ccae1fda7c",
+    "url": "/static/js/main.03893f56.chunk.js"
+  },
+  {
+    "revision": "99f83ca6a189d128bb4a",
+    "url": "/static/js/runtime-main.d7883c2b.js"
+  },
+  {
+    "revision": "5ab012741e717cda3e641ad0da58214f",
+    "url": "/static/media/skype_remix_2.5ab01274.mp3"
+  }
+]);
